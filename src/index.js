e33bf398d895132f90e0caa83ae9e076e085654d
Justin Tormey <justin@blockchain.com>
2017-03-14 17:07:08 -0400

    chore(Package): package with support for es5

---
+++ src/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Exchange: require('./exchange'),
+  API: require('./api'),
+  PaymentAccount: require('./payment-account'),
+  PaymentMedium: require('./payment-medium'),
+  Quote: require('./quote'),
+  Trade: require('./trade'),
+  Helpers: require('./helpers')
+};
